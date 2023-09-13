4015ef1b1c3da9352f1755e8e93fe5257853f994
João Lessa <joaovictorlessa98@gmail.com>
2023-09-13 16:48:07 -0300

    Testes com sendkeys

--- src/test/java/qaacademy/PrincipaisComandosSelenium.java
+++ src/test/java/qaacademy/PrincipaisComandosSelenium.java
@@ -20,4 +20,24 @@ public class PrincipaisComandosSelenium {
          driver.findElement(By.xpath("//*[@id=\'dataItem-k3hb7c5j-comp-k0wqferu\']/a")).click();
         
     }
+
+    @Test
+    public void testeSeleniumEnviarTextosParaTela() throws InterruptedException{
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qaacademy.com.br/");
+
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/button[1]")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Nome']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Nome']")).sendKeys("João Lessa");
+
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("joaovictor@gmail.com");
+
+        driver.findElement(By.xpath("//span[contains(text(),'Enviar')]")).click();
+        
+    }
 }
